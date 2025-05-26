4b1b79f420d98002d9f5555058df0f046b7dd1f6
ifsabhi64 <ifsabhi64@gmail.com>
2025-05-26 08:57:17 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,10 +3,10 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28094744"))
-API_HASH = environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
+API_ID = int(environ.get("API_ID", "20567114)
+API_HASH = environ.get("API_HASH", "8a5b92106e45fc6637a65a67df060a65")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7515462044:AAHPk-v697dn5NNeJU81FewANQoq-b8rKF8")
+OWNER = int(environ.get("OWNER", "8036182138"))
+CREDIT = "༄ᶦᶰᵈआशु࿐❥◉🇮🇳™"
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
